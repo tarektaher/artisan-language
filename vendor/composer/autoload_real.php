60390323c1b9412943abb71f13254708f7158af6
tarektaher <tarektaher@live.fr>
2024-11-28 13:05:07 +0100

    Préparation pour la version 0.1.0

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitaec0c5ddfd322d493e83670fc4ccc50a
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitaec0c5ddfd322d493e83670fc4ccc50a', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitaec0c5ddfd322d493e83670fc4ccc50a', 'loadClassLoader'));
